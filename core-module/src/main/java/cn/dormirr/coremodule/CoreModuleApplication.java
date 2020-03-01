d69f5e2ceaf4fa3acb4a95dc01a270dc4a481389
dormirr <1018729292@qq.com>
2020-03-01 13:10:58 +0800

    Initial commit

---
+++ core-module/src/main/java/cn/dormirr/coremodule/CoreModuleApplication.java
@@ -0,0 +1,13 @@
+package cn.dormirr.coremodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoreModuleApplication.class, args);
+    }
+
+}
---
+++ core-module/src/test/java/cn/dormirr/coremodule/CoreModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.dormirr.coremodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
