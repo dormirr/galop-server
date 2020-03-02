9ee709f94eecf6564e4cae8a1d9e37980a7fddaf
dormirr <1018729292@qq.com>
2020-03-02 14:15:14 +0800

    创建登录验证类
    创建登录验证类

---
+++ core-module/src/main/java/cn/dormirr/coremodule/security/service/UserDetailsServiceImpl.java
@@ -0,0 +1,34 @@
+package cn.dormirr.coremodule.security.service;
+
+import cn.dormirr.coremodule.role.service.UserService;
+import cn.dormirr.coremodule.role.service.dto.UserDto;
+import cn.dormirr.coremodule.security.security.vo.JwtUser;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author ZhangTianCi
+ */
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserService userService;
+
+    public UserDetailsServiceImpl(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String userNumber) {
+        UserDto userDto = userService.findByUserNumber(userNumber);
+        if (userDto == null) {
+            //TODO 抛出账号不存在异常
+        }
+        return createJwtUser(userDto);
+    }
+
+    private JwtUser createJwtUser(UserDto userDto) {
+        return new JwtUser(userDto);
+    }
+}
