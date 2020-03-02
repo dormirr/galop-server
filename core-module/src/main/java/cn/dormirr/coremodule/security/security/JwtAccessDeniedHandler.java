5c9b41bac7f4d87e1557390bb536293995447311
dormirr <1018729292@qq.com>
2020-03-02 13:56:47 +0800

    创建权限错误处理
    创建身份验证错误处理；
    创建访问被拒绝处理。

---
+++ core-module/src/main/java/cn/dormirr/coremodule/security/security/JwtAccessDeniedHandler.java
@@ -0,0 +1,26 @@
+package cn.dormirr.coremodule.security.security;
+
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author ZhangTianCi
+ */
+@Component
+public class JwtAccessDeniedHandler implements AccessDeniedHandler {
+
+    @Override
+    public void handle(HttpServletRequest request,
+                       HttpServletResponse response,
+                       AccessDeniedException accessDeniedException
+    ) throws IOException {
+
+        //当用户在没有授权的情况下访问受保护的 REST 资源时，将调用此方法发送 403 Forbidden 响应
+        response.sendError(HttpServletResponse.SC_FORBIDDEN, accessDeniedException.getMessage());
+    }
+}
---
+++ core-module/src/main/java/cn/dormirr/coremodule/security/security/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,28 @@
+package cn.dormirr.coremodule.security.security;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author ZhangTianCi
+ */
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException authException
+    ) throws IOException {
+
+        // 当用户尝试访问安全的 REST 资源而不提供任何凭据时，将调用此方法发送 401 响应
+        response.sendError(
+                HttpServletResponse.SC_UNAUTHORIZED, authException == null ? "未经授权" : authException.getMessage()
+        );
+    }
+}
