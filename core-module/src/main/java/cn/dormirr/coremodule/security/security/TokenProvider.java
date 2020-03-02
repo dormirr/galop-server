204dfa420896d9f0dc84ec8e2b87db20f1738765
dormirr <1018729292@qq.com>
2020-03-02 14:37:17 +0800

    创建 Token 提供类
    创建 Token 提供类。

---
+++ core-module/src/main/java/cn/dormirr/coremodule/security/security/TokenProvider.java
@@ -0,0 +1,129 @@
+package cn.dormirr.coremodule.security.security;
+
+import cn.dormirr.coremodule.security.config.SecurityProperties;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import java.security.Key;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Date;
+import java.util.stream.Collectors;
+
+/**
+ * 提供 Token
+ *
+ * @author ZhangTianCi
+ */
+@Slf4j
+@Component
+public class TokenProvider implements InitializingBean {
+
+    private final SecurityProperties properties;
+    private static final String AUTHORITIES_KEY = "auth";
+    private Key key;
+
+    public TokenProvider(SecurityProperties properties) {
+        this.properties = properties;
+    }
+
+    @Override
+    public void afterPropertiesSet() {
+        byte[] keyBytes = Decoders.BASE64.decode(properties.getBase64Secret());
+        this.key = Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    /**
+     * 创建 Token
+     *
+     * @param authentication 认证方式
+     * @return Token
+     */
+    public String createToken(Authentication authentication) {
+        String authorities = authentication.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(","));
+
+        long now = (new Date()).getTime();
+        Date validity = new Date(now + properties.getTokenValidityInSeconds());
+
+        return Jwts.builder()
+                .setSubject(authentication.getName())
+                .claim(AUTHORITIES_KEY, authorities)
+                .signWith(key, SignatureAlgorithm.HS512)
+                .setExpiration(validity)
+                .compact();
+    }
+
+    /**
+     * 获取认证
+     *
+     * @param token 令牌
+     * @return 认证
+     */
+    Authentication getAuthentication(String token) {
+        Claims claims = Jwts.parser()
+                .setSigningKey(key)
+                .parseClaimsJws(token)
+                .getBody();
+
+        Collection<? extends GrantedAuthority> authorities =
+                Arrays.stream(claims.get(AUTHORITIES_KEY).toString().split(","))
+                        .map(SimpleGrantedAuthority::new)
+                        .collect(Collectors.toList());
+
+        User principal = new User(claims.getSubject(), "", authorities);
+
+        return new UsernamePasswordAuthenticationToken(principal, token, authorities);
+    }
+
+    /**
+     * 验证 Token
+     *
+     * @param authToken Token
+     * @return 验证结果
+     */
+    boolean validateToken(String authToken) {
+        try {
+            Jwts.parser().setSigningKey(key).parseClaimsJws(authToken);
+            return true;
+        } catch (io.jsonwebtoken.security.SecurityException | MalformedJwtException e) {
+            log.info("无效的JWT签名。");
+            e.printStackTrace();
+        } catch (ExpiredJwtException e) {
+            log.info("JWT令牌已过期。");
+            e.printStackTrace();
+        } catch (UnsupportedJwtException e) {
+            log.info("不支持的JWT令牌。");
+            e.printStackTrace();
+        } catch (IllegalArgumentException e) {
+            log.info("处理程序的JWT令牌契约无效。");
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    /**
+     * 获取 Token
+     *
+     * @param request 请求
+     * @return Token 前缀
+     */
+    public String getToken(HttpServletRequest request) {
+        final String requestHeader = request.getHeader(properties.getHeader());
+        if (requestHeader != null && requestHeader.startsWith(properties.getTokenStartWith())) {
+            return requestHeader.substring(7);
+        }
+        return null;
+    }
+}
