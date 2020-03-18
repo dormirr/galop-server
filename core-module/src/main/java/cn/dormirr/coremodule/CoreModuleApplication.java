8f59569071ee807992819abf0ab9f755cb1cb62f
dormirr <1018729292@qq.com>
2020-03-18 20:13:06 +0800

    添加配置
    扩大扫描范围；
    添加配置。

--- core-module/src/main/java/cn/dormirr/coremodule/CoreModuleApplication.java
+++ core-module/src/main/java/cn/dormirr/coremodule/CoreModuleApplication.java
@@ -1,13 +1,27 @@
 package cn.dormirr.coremodule;
 
+import cn.dormirr.commonmodule.utils.SpringContextHolder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+/**
+ * @author ZhangTianCi
+ */
+@EnableAsync
 @SpringBootApplication
+@ComponentScan(basePackages = {"cn.dormirr"})
 public class CoreModuleApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CoreModuleApplication.class, args);
+    }
+
+    @Bean
+    public SpringContextHolder springContextHolder() {
+        return new SpringContextHolder();
     }
 
 }
