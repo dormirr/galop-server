47b3f74a3981e0e5b84a2f9b60ab7a0db06daa92
dormirr <1018729292@qq.com>
2020-03-18 17:28:06 +0800

    修改 JwtUser
    修改 JwtUser 属性。

--- core-module/src/main/java/cn/dormirr/coremodule/security/security/vo/JwtUser.java
+++ core-module/src/main/java/cn/dormirr/coremodule/security/security/vo/JwtUser.java
@@ -1,5 +1,7 @@
 package cn.dormirr.coremodule.security.security.vo;
 
+import cn.dormirr.coremodule.role.domain.Role;
+import cn.dormirr.coremodule.role.domain.User;
 import cn.dormirr.coremodule.role.service.dto.UserDto;
 import lombok.Data;
 import org.springframework.security.core.GrantedAuthority;
@@ -12,29 +14,100 @@ import java.util.Collection;
 /**
  * @author ZhangTianCi
  */
-@Data
 public class JwtUser implements UserDetails {
-    private UserDto userDto;
+
+    private String userNumber;
+    private String userName;
+    private String userPassword;
+    private String userEmail;
+    private String userPortrait;
+    private Integer userFightingCapacity;
+    private Role roleByRoleId;
 
     public JwtUser(UserDto userDto) {
-        this.userDto = userDto;
+        this.userNumber = userDto.getUserNumber();
+        this.userName = userDto.getUserName();
+        this.userPassword = userDto.getUserPassword();
+        this.userEmail = userDto.getUserEmail();
+        this.userPortrait = userDto.getUserPortrait();
+        this.userFightingCapacity = userDto.getUserFightingCapacity();
+        this.roleByRoleId = userDto.getRoleByRoleId();
+    }
+
+    public JwtUser() {
+    }
+
+    public String getUserNumber() {
+        return userNumber;
+    }
+
+    public void setUserNumber(String userNumber) {
+        this.userNumber = userNumber;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUserPortrait() {
+        return userPortrait;
+    }
+
+    public void setUserPortrait(String userPortrait) {
+        this.userPortrait = userPortrait;
+    }
+
+    public Integer getUserFightingCapacity() {
+        return userFightingCapacity;
+    }
+
+    public void setUserFightingCapacity(Integer userFightingCapacity) {
+        this.userFightingCapacity = userFightingCapacity;
+    }
+
+    public Role getRoleByRoleId() {
+        return roleByRoleId;
+    }
+
+    public void setRoleByRoleId(Role roleByRoleId) {
+        this.roleByRoleId = roleByRoleId;
     }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         Collection<GrantedAuthority> authorities = new ArrayList<>();
-        authorities.add(new SimpleGrantedAuthority(userDto.getRoleByRoleId().getRoleName()));
+        authorities.add(new SimpleGrantedAuthority(getRoleByRoleId().getRoleName()));
         return authorities;
     }
 
     @Override
     public String getPassword() {
-        return userDto.getUserPassword();
+        return getUserPassword();
     }
 
     @Override
     public String getUsername() {
-        return userDto.getUserNumber();
+        return getUserNumber();
     }
 
     @Override
