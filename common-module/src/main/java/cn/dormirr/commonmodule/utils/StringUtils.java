1edeaba375ad63f5641018178117d92522755439
dormirr <1018729292@qq.com>
2020-03-22 15:02:51 +0800

    创建工具类
    创建字符串工具类；
    创建加密工具类；
    创建文件工具类；
    创建 Redis 工具类；
    创建安全验证工具类。

---
+++ common-module/src/main/java/cn/dormirr/commonmodule/utils/StringUtils.java
@@ -0,0 +1,10 @@
+package cn.dormirr.commonmodule.utils;
+
+/**
+ * 字符串工具类, 继承org.apache.commons.lang3.StringUtils类
+ *
+ * @author ZhangTianCi
+ */
+public class StringUtils extends org.apache.commons.lang3.StringUtils {
+
+}
