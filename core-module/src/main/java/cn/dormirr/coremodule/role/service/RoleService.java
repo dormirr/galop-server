753975b756288f185deb79e5ea66ce5a0ae099ec
dormirr <1018729292@qq.com>
2020-03-22 14:57:50 +0800

    创建业务接口与实现
    创建根据角色名查询角色接口与实现。

--- core-module/src/main/java/cn/dormirr/coremodule/role/service/RoleService.java
+++ core-module/src/main/java/cn/dormirr/coremodule/role/service/RoleService.java
@@ -1,8 +1,17 @@
 package cn.dormirr.coremodule.role.service;
 
+import cn.dormirr.coremodule.role.service.dto.RoleDto;
+
 /**
  * @author ZhangTianCi
  */
 public interface RoleService {
 
+    /**
+     * 根据角色名查询角色
+     *
+     * @param roleName 角色名
+     * @return 角色
+     */
+    RoleDto findByRoleName(String roleName);
 }
--- core-module/src/main/java/cn/dormirr/coremodule/role/service/impl/RoleServiceImpl.java
+++ core-module/src/main/java/cn/dormirr/coremodule/role/service/impl/RoleServiceImpl.java
@@ -1,6 +1,10 @@
 package cn.dormirr.coremodule.role.service.impl;
 
+import cn.dormirr.coremodule.role.domain.Role;
+import cn.dormirr.coremodule.role.repository.RoleRepository;
 import cn.dormirr.coremodule.role.service.RoleService;
+import cn.dormirr.coremodule.role.service.dto.RoleDto;
+import cn.dormirr.coremodule.role.service.mapper.RoleMapper;
 import org.springframework.stereotype.Service;
 
 /**
@@ -8,5 +12,26 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class RoleServiceImpl implements RoleService {
+    private final RoleRepository roleRepository;
+    private final RoleMapper roleMapper;
 
+    public RoleServiceImpl(RoleRepository roleRepository, RoleMapper roleMapper) {
+        this.roleRepository = roleRepository;
+        this.roleMapper = roleMapper;
+    }
+
+    /**
+     * 根据角色名查询角色
+     *
+     * @param roleName 角色名
+     * @return 角色DTO
+     */
+    @Override
+    public RoleDto findByRoleName(String roleName) {
+        Role role = roleRepository.findByRoleName(roleName);
+        if (role != null) {
+            return roleMapper.toDto(role);
+        }
+        return null;
+    }
 }
