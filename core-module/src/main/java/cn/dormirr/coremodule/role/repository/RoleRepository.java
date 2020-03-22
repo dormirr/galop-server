2e62256df34c1e2f519b3c245a63cc2471ea4343
dormirr <1018729292@qq.com>
2020-03-22 14:57:01 +0800

    创建查询接口
    创建根据角色名查询角色接口。

--- core-module/src/main/java/cn/dormirr/coremodule/role/repository/RoleRepository.java
+++ core-module/src/main/java/cn/dormirr/coremodule/role/repository/RoleRepository.java
@@ -8,4 +8,12 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
  * @author ZhangTianCi
  */
 public interface RoleRepository extends JpaRepository<Role, Long>, JpaSpecificationExecutor<Role> {
+
+    /**
+     * 根据角色名查询角色
+     *
+     * @param roleName 角色名
+     * @return 角色
+     */
+    Role findByRoleName(String roleName);
 }
