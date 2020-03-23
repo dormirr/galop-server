69d338dc33631168bdf991781642b11a2eab05a5
dormirr <1018729292@qq.com>
2020-03-23 17:37:57 +0800

    添加 VO
    添加 VO。

---
+++ core-module/src/main/java/cn/dormirr/coremodule/role/domain/vo/UserNameAndEmail.java
@@ -0,0 +1,14 @@
+package cn.dormirr.coremodule.role.domain.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author ZhangTianCi
+ */
+@Getter
+@Setter
+public class UserNameAndEmail {
+    private String userName;
+    private String userEmail;
+}
---
+++ core-module/src/main/java/cn/dormirr/coremodule/role/domain/vo/UserPassword.java
@@ -0,0 +1,13 @@
+package cn.dormirr.coremodule.role.domain.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author ZhangTianCi
+ */
+@Getter
+@Setter
+public class UserPassword {
+    private String userPassword;
+}
