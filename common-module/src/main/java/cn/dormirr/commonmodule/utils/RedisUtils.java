ab81e1abd454042f2e29638842d675d464ce077b
dormirr <1018729292@qq.com>
2020-04-05 13:58:50 +0800

    :art: 优化导入

--- common-module/src/main/java/cn/dormirr/commonmodule/utils/RedisUtils.java
+++ common-module/src/main/java/cn/dormirr/commonmodule/utils/RedisUtils.java
@@ -3,7 +3,10 @@ package cn.dormirr.commonmodule.utils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.connection.RedisConnection;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.*;
+import org.springframework.data.redis.core.Cursor;
+import org.springframework.data.redis.core.RedisConnectionUtils;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ScanOptions;
 import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
 
